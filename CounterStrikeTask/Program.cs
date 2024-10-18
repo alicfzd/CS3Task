4225a36e0060312196e1ccbd6e5d4785d612b95d
alicfzd <alinc-ap117@code.edu.az>
2024-10-19 00:32:29 +0400

    CS3CIXDI!

---
+++ CounterStrikeTask/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterStrikeTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(32, 32, 2.9f, "Auto");
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("0 - Get Information");
+                Console.WriteLine("1 - Shoot");
+                Console.WriteLine("2 - Fire");
+                Console.WriteLine("3 - Get Remaining Bullet Count");
+                Console.WriteLine("4 - Reload");
+                Console.WriteLine("5 - Change Fire Mode");
+                Console.WriteLine("6 - Exit");
+                Console.Write("Select an option (0-6): ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 0:
+
+                            Console.WriteLine("Information: \n- Shoot: bir gulle at.\n- Fire: butun gulleleri at.\n- Get Remaining Bullet Count: Qalan gulle sayina bax.\n- Reload: Yeniden doldur.\n- Change Fire Mode: Atis modunu deyis.");
+                            break;
+
+                        case 1:
+                            {
+                                weapon.Shoot();
+                                break;
+                            }
+
+                        case 2:
+                            weapon.Fire();
+                            break;
+
+                        case 3:
+                            Console.WriteLine(weapon.GetRemainBullets());
+                            break;
+
+                        case 4:
+                            weapon.Reload();
+                            break;
+
+                        case 5:
+                            weapon.ChangeFireMode();
+                            break;
+
+                        case 6:
+                            Console.WriteLine("Proqram deyandirildi");
+                            running = false;
+                            break;
+                     }
+                    }
+                else
+                {
+                    Console.WriteLine("Yalnis eded");
+                }
+            }
+        }
+    }
+}
---
+++ CounterStrikeTask/Weapon.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CounterStrikeTask
+{
+    public class Weapon
+    {
+        public int BulletCap { get; set; }
+        public int CurrentBullets { get; set; }
+        public float DischargeTime { get; set; }
+        public string FireMod { get; set; }
+
+        public Weapon()
+        {
+
+        }
+
+        public Weapon(int bulletCapacity, int currentBulletsCount, float dischargeTime, string fireMod)
+        {
+            BulletCap = bulletCapacity;
+            CurrentBullets = currentBulletsCount;
+            DischargeTime = dischargeTime;
+            FireMod = fireMod;
+        }
+
+        public void Shoot()
+        {
+            if (CurrentBullets > 0)
+            {
+                CurrentBullets--; 
+                Console.WriteLine("Bir defe ates edildi");
+                Console.WriteLine($"Gulle sayi {CurrentBullets}/{BulletCap}");
+            }
+            else
+            {
+                Console.WriteLine("Daraq bosdur");
+                Reload();
+
+            }
+
+        }
+
+        public void Fire()
+        {
+            if (CurrentBullets > 0)
+            {
+                if (FireMod == "Auto")
+                {
+                    Console.WriteLine("Ates basladi");
+
+                    float spentTime = (CurrentBullets * DischargeTime) / BulletCap;                    
+
+                    Console.WriteLine("Ates bitdi!");
+
+                    Console.WriteLine($"{CurrentBullets} gulle istifade edildi. Ates {spentTime} saniye davam etdi");
+
+                    CurrentBullets = 0;
+
+                }
+                else
+                {
+                    Shoot();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Daraq bosdur");
+                Console.Write("Daragi deyisdirmek isteyirsiniz mi?(y/n):");
+                string code = default;
+                bool running = true;
+                while (running)
+                {
+                    code = Console.ReadLine();
+                    if (code == "y")
+                    {
+                        Reload();
+                        running = false;
+                    }
+                    else if (code == "n")
+                    {
+                        Console.WriteLine("Silahin daragi bos olaraq qalir!");
+                        running = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Duzgun code daxil edilmedi!!!");
+                        Console.Write("Daragi deyisdirmek isteyirsiniz mi?(y/n):");
+                    }
+                }
+
+            }
+        }
+
+        public int GetRemainBullets()
+        {
+            int remainBulletsCount = BulletCap - CurrentBullets;
+            return remainBulletsCount;
+        }
+
+        public void Reload()
+        {
+            if (CurrentBullets != BulletCap)
+            {
+                Console.WriteLine("Daraq deyisdirildi.");
+                CurrentBullets = BulletCap;
+            }
+            else
+            {
+                Console.WriteLine("Daraq deyismeye ehtiyac yoxdur");
+            }
+        }
+
+        public void ChangeFireMode()
+        {
+            if (FireMod == "Auto")
+            {
+                FireMod = "Single";
+            }
+            else
+            {
+                FireMod = "Auto";
+            }
+
+            Console.WriteLine($"Atis modu {FireMod} olaraq deyisdirildi");
+        }
+    }
+}
